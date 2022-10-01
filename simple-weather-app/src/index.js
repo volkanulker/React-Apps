e53bfd7b8305dce3cf6c86efef1261860d8e49d3
Volkan Ulker <53200043+volkanulker@users.noreply.github.com>
2022-10-01 13:55:19 +0300

    Weathers project has been completed.

--- simple-weather-app/src/index.js
+++ simple-weather-app/src/index.js
@@ -1,15 +1,34 @@
-import React from 'react'
-import  ReactDOM  from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-const App = () => {
+class App extends React.Component {
+    state = { lat: null, errorMessage: "" };
 
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
+        );
+    }
 
-    return (
-        <div>
-            <h1>Latitude: </h1>
-        </div>
-    )
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />;
+        }
+
+        return <Spinner message="Please accept location request" />;
+    }
+
+    render() {
+        return <div>{this.renderContent()}</div>;
+    }
 }
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
-root.render(<App/>)
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<App />);
